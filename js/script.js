4a8b5c922a69f506df5e05ba52ddb60ed4d0a72d
Santiago Guerra <guerrasantiago19@gmail.com>
2020-02-25 14:10:00 -0500

    generating rows manually

--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ class MyLibrary {
     this.books = array;
   }
 }
+
+const myLibrary = new MyLibrary([new Book('Name Book', 'Author Name', 434), new Book('Name Book', 'Author Name', 434), new Book('Name Book', 'Author Name', 434)]);
+
+const table = document.getElementById('table');
+const createTableRow = book => {
+  const row = document.createElement('tr');
+
+  const dataRow = `
+  <td>${book.name}</td>
+  <td>${book.author}</td>
+  <td>${book.pages}</td>
+  <td>${book.isReaded}</td>
+  <td></td>
+  `;
+
+  row.insertAdjacentHTML('beforeend', dataRow);
+
+  return row;
+};
+
+table.appendChild(createTableRow(myLibrary.books[0]));
