7e36fbb5797baf2f7e7fadb97eb5253b4694fb79
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-03-22 20:24:24 -0300

    Refactor code: more organized

---
+++ js/Book.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+class Book {
+  constructor(id, name, author, pages, isRead) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.id = id;
+  }
+}
